c56fd08031cce62f4590e71cc5b141c6535baee9
oda.nobunaga <smtsmbam3s13@gmail.com>
2021-12-09 12:23:23 +0000

    fizzbuzz

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,15 @@
+num_max = 100
+(1..num_max).each do |num|
+def fizzbuzz(num)
+   if num % 3 == 0 && num % 5 == 0 
+    return 'FizzBuzz' 
+   elsif num % 5 == 0 
+    return 'Buzz'
+   elsif num % 3 == 0
+    return 'Fizz'
+   else
+    return num
+   end
+end
+   puts fizzbuzz(num)
+end
